35124a44676aea91b1ce9cdfab439a5b919033d4
Yousef-El-Black <yousefelblackdev@gmail.com>
2023-05-08 01:20:15 +0300

    Update

--- scandiweb-server/config/db_connect.php
+++ scandiweb-server/config/db_connect.php
@@ -1,7 +1,10 @@
 <?php
-$database = 'scandiweb';
-$username = 'youssef';
-$password = 'test1234';
+$database = 'id20712597_scandiweb	';
+$testdatabase = 'scandiweb';
+$username = 'id20712597_youssef';
+$testusername = 'youssef';
+$password = 'ilM1=1Cn+]Nk^cPx';
+$testpassword = 'test1234';
 $hostname = 'localhost';
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
